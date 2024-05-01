b3a4ed0a6b76f5cd39c07968a96670083b86851f
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-05-01 08:32:02 +0300

    Add Constants

---
+++ Daskalo.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,31 @@
+﻿namespace Daskalo.Infrastructure.Constants
+{
+    /// <summary>
+    /// Contains constants about the data models.
+    /// </summary>
+    public static class DataConstants
+    {
+        public const int UserFullNameMinLength = 5;
+        public const int UserFullNameMaxLength = 40;
+
+        public const int GroupNameMinLength = 2;
+        public const int GroupNameMaxLength = 100;
+        public const int GroupShortNameMinLength = 2;
+        public const int GroupShortNameMaxLength = 10;
+        public const int GroupDescriptionMaxLength = 200;
+
+        public const int ResourceTextToDisplayMinLength = 3;
+        public const int ResourceTextToDisplayMaxLength = 70;
+
+        public const int SchoolNameMinLength = 10;
+        public const int SchoolNameMaxLength = 150;
+        public const int SchoolTypeMinLength = 2;
+        public const int SchoolTypeMaxLength = 7;
+        public const int SchoolCityMinLength = 3;
+        public const int SchoolCityMaxLength = 15;
+
+        public const int TopicNameMinLength = 2;
+        public const int TopicNameMaxLength = 100;
+        public const int TopicDescriptionMaxLength = 200;
+    }
+}
---
+++ Daskalo.Infrastructure/Constants/ErrorMessages.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Daskalo.Infrastructure.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string StringLengthErrorMessage = "Полето {0} трябва да има дължина от {2} до {1} символа.";
+        public const string MaxLengthErrorMessage = "Полето {0} трябва да има поне {1} символа.";
+        public const string RequiredErrorMessage = "Полето {0} е задължително.";
+
+        public const string SchoolEntityNotFoundErrorMessage = "Училище с id: {0} не беше открито.";
+
+        public const string FileITypeErrorMessage = "Грешен тип на файл: {0}.";
+        public const string FileISizeErrorMessage = "Размерът на файла трябва да е до {0}KB.";
+
+    }
+}
