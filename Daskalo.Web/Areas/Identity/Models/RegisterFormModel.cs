21b5c3e8d9b5ee102feebaf074136f70c5acbbcf
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-05-05 08:14:34 +0300

    Edit registration page

---
+++ Daskalo.Web/Areas/Identity/Models/RegisterFormModel.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Daskalo.Infrastructure.Data.Constants.DataConstants;
+using static Daskalo.Infrastructure.Data.Constants.ErrorMessages;
+
+namespace Daskalo.Web.Areas.Identity.Models
+{
+    public class RegisterFormModel
+    {
+        /// <summary>
+        /// Use this property to pass created Application User identifier.
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [EmailAddress]
+        [Display(Name = "Имейл")]
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(100, ErrorMessage = "{0}та трябва да съдържа от {2} до {1} символа.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Парола")]
+        public string Password { get; set; } = string.Empty;
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Потвърждение на паролата")]
+        [Compare("Password", ErrorMessage = "Паролата и потвърждението на паролата не съвпадат.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The full name of the user.
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(
+            UserFullNameMaxLength,
+            MinimumLength = UserFullNameMinLength,
+            ErrorMessage = StringLengthErrorMessage)]
+        [Display(Name = "Име, презиме и фамилия")]
+        public string FullName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The name of the school the user wants to register in.
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [Display(Name = "Училище")]
+        public string SchoolId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The role which the user wants to participate on.
+        /// </summary>
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [Display(Name = " Роля")]
+        public string Role { get; set; } = string.Empty;
+    }
+}
--- Daskalo.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Daskalo.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,40 +2,39 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
+using Daskalo.Core.Contracts;
+using Daskalo.Infrastructure.Data.Models;
+using Daskalo.Web.Areas.Identity.Models;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
 
-namespace Daskalo.Web.Areas.Identity.Pages.Account
+namespace Web.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly IUserStore<IdentityUser> _userStore;
-        private readonly IUserEmailStore<IdentityUser> _emailStore;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserStore<ApplicationUser> _userStore;
+        private readonly IUserEmailStore<ApplicationUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
 
+        private readonly ISchoolService _schoolService;
+        private readonly ITeacherService _teacherService;
+        private readonly IStudentService _studentService;
+
         public RegisterModel(
-            UserManager<IdentityUser> userManager,
-            IUserStore<IdentityUser> userStore,
-            SignInManager<IdentityUser> signInManager,
+            UserManager<ApplicationUser> userManager,
+            IUserStore<ApplicationUser> userStore,
+            SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            ISchoolService schoolService,
+            ITeacherService teacherService,
+            IStudentService studentService)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -43,6 +42,9 @@ namespace Daskalo.Web.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _schoolService = schoolService;
+            _teacherService = teacherService;
+            _studentService = studentService;
         }
 
         /// <summary>
@@ -50,7 +52,7 @@ namespace Daskalo.Web.Areas.Identity.Pages.Account
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         [BindProperty]
-        public InputModel Input { get; set; }
+        public RegisterFormModel Input { get; set; }
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -64,44 +66,11 @@ namespace Daskalo.Web.Areas.Identity.Pages.Account
         /// </summary>
         public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public class InputModel
-        {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
-            public string Email { get; set; }
-
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
-            public string Password { get; set; }
-
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
-        }
-
-
         public async Task OnGetAsync(string returnUrl = null)
         {
+            var allSchools = await _schoolService.AllAsync();
+            //Input = new RegisterFormModel();
+
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
@@ -109,13 +78,16 @@ namespace Daskalo.Web.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
+
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                user.FullName = Input.FullName;
+                user.SchoolId = Input.SchoolId;
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
@@ -123,26 +95,21 @@ namespace Daskalo.Web.Areas.Identity.Pages.Account
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    Input.Id = userId;
+                    switch (Input.Role)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                        case "Student":
+                            //await _studentService.CreateAsync(Input);
+                            break;
+                        case "Teacher":
+                            //await _teacherService.CreateAsync(Input);
+                            break;
+                        default:
+                            break;
                     }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
                 {
@@ -150,15 +117,16 @@ namespace Daskalo.Web.Areas.Identity.Pages.Account
                 }
             }
 
-            // If we got this far, something failed, redisplay form
+            //var allSchools = await _schoolService.AllAsync();
+            //Input = new RegisterFormModel();
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
@@ -168,13 +136,13 @@ namespace Daskalo.Web.Areas.Identity.Pages.Account
             }
         }
 
-        private IUserEmailStore<IdentityUser> GetEmailStore()
+        private IUserEmailStore<ApplicationUser> GetEmailStore()
         {
             if (!_userManager.SupportsUserEmail)
             {
                 throw new NotSupportedException("The default UI requires a user store with email support.");
             }
-            return (IUserEmailStore<IdentityUser>)_userStore;
+            return (IUserEmailStore<ApplicationUser>)_userStore;
         }
     }
 }
